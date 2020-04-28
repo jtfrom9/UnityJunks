759869f4fd92aa62d47f9ff8da1bad9afa84d92b
Jun Tachikawa <jtachikawa@xevo.com>
2020-04-28 11:40:33 +0900

    add EditorStatus

---
+++ Assets/EditorStatus.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[InitializeOnLoad]
+public static class PlaymodeStateObserver
+{
+    static PlaymodeStateObserver()
+    {
+        UnityEditor.EditorApplication.playModeStateChanged += (state) => {
+            Debug.Log(state);
+        };
+    }
+}
+
+public class EditorStatus: MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Start");
+    }
+    void OnApplicationQuit()
+    {
+        Debug.Log("OnApplicationQuit");
+    }
+    void OnApplicationPause()
+    {
+        Debug.Log("OnApplicationPause");
+    }
+}
