7462c4384d29e0ca6e985b2b7035323e9fedefe6
Jun Tachikawa <jtachikawa@xevo.com>
2020-04-30 20:29:28 +0900

    added uGUI

---
+++ Assets/uGUIController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx.Async;
+using UniRx.Async.Triggers;
+
+public class uGUIController : MonoBehaviour
+{
+    Button button;
+
+    async void Start()
+    {
+        var token = this.GetCancellationTokenOnDestroy();
+        await button.GetAsyncClickEventHandler(token).OnClickAsync();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
