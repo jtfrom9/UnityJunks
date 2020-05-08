4192c9c4fa86911a85fc16efda1ec87cce97b71f
Jun Tachikawa <jtachikawa@xevo.com>
2020-05-08 09:07:03 +0900

    add Slide Animation

---
+++ Assets/Slidein.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+
+public class Slidein : MonoBehaviour
+{
+    public Button button;
+    public Animator animator;
+
+    void Start()
+    {
+        button.OnClickAsObservable().Subscribe(_ => {
+            Debug.Log("click");
+            var b = animator.GetBool("SlideIn");
+            animator.SetBool("SlideIn", !b);
+        });
+    }
+}
