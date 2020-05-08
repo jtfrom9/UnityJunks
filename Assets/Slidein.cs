0e9143c4a2561e66cd74153fd47576da6af8dd32
Jun Tachikawa <jtachikawa@xevo.com>
2020-05-08 18:15:10 +0900

    add iTween slide

--- Assets/Slidein.cs
+++ Assets/Slidein.cs
@@ -6,15 +6,33 @@ using UniRx;
 
 public class Slidein : MonoBehaviour
 {
-    public Button button;
+    public Button buttonMecanim;
     public Animator animator;
+    public Button buttonITween;
+    public GameObject panelITween;
 
     void Start()
     {
-        button.OnClickAsObservable().Subscribe(_ => {
+        // Mecanim
+        buttonMecanim.OnClickAsObservable().Subscribe(_ =>
+        {
             Debug.Log("click");
             var b = animator.GetBool("SlideIn");
             animator.SetBool("SlideIn", !b);
+        });
+
+        // iTween
+        var rtrans = panelITween.GetComponent<RectTransform>();
+        rtrans.anchoredPosition += new Vector2(2000, 0);
+
+        bool slideOut = true;
+        buttonITween.OnClickAsObservable().Subscribe(_ =>
+        {
+            iTween.MoveTo(panelITween, iTween.Hash(
+                "x", (slideOut) ? 0f : 2000f,
+                "islocal", true
+            ));
+            slideOut = !slideOut;
         });
     }
 }
