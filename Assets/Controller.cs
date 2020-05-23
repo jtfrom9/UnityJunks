ff5d42930ad439078edb5bc075c779865f9c6550
Jun Tachikawa <jtachikawa@xevo.com>
2020-05-24 02:31:08 +0900

    add a GameObject routine test

---
+++ Assets/Controller.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Controller : MonoBehaviour
+{
+    void Awake()
+    {
+        Debug.Log("Awake");
+    }
+    void OnEanble()
+    {
+        Debug.Log("OnEanble");
+    }
+    void Start()
+    {
+        Debug.Log("Start");
+    }
+    void OnDisable()
+    {
+        Debug.Log("OnDisable");
+    }
+    void OnDestroy()
+    {
+        Debug.Log("OnDestroy");
+    }
+}
