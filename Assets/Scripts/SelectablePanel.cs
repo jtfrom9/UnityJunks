33120abcd63c376bae5fc50f235eafe8da590586
jtfrom9 <jun.tachikawa@gmail.com>
2020-09-02 14:05:23 +0900

    Selectable Panel List

---
+++ Assets/Scripts/SelectablePanel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class SelectablePanel : MonoBehaviour
+{
+    [SerializeField] Toggle toggle;
+    public void Submit()
+    {
+        toggle.isOn = !toggle.isOn;
+    }
+}
+
